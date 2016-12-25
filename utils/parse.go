9e1b7a7502b5da1b5e8dc2653f4d9b64599651e4
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-12-25 02:40:52 -0200

    Added a Filer interface and implemented the interface for two different types and used the interface in parse.go instead of interface{}

--- utils/parse.go
+++ utils/parse.go
@@ -1,34 +1,35 @@
-package utils;
+package utils
 
 import (
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
 
-func ParseYamlFile(filePath string, file interface{}) {
-    yamlFile, err := ioutil.ReadFile(filePath)
+func ParseYamlFile(filePath string, file Filer) {
+	yamlFile, err := ioutil.ReadFile(filePath)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    err = yaml.Unmarshal(yamlFile, file)
+	err = yaml.Unmarshal(yamlFile, file)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
-func SaveYamlFile(filePath string, file interface{}) {
-    yamlByte, err := yaml.Marshal(file)
+func SaveYamlFile(filePath string, file Filer) {
+	yamlByte, err := yaml.Marshal(file)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    err = ioutil.WriteFile(filePath, yamlByte, 0644)
+	err = ioutil.WriteFile(filePath, yamlByte, 0644)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
